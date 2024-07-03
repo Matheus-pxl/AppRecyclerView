af544d63abd5a0fd74c8b732e7081a3aac1e72ac
Econsoft <matheushellf@gmail.com>
2024-07-03 11:25:12 -0300

    comentarios e inicialização do recyclerView na main activity

--- app/src/main/java/com/app/MainActivity.kt
+++ app/src/main/java/com/app/MainActivity.kt
@@ -1,20 +1,27 @@
 package com.app
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.app.adapter.JogosAdapter
+import com.app.databinding.ActivityMainBinding
+import com.app.datasource.Datasource
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding // Declaração do binding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        iniciarRecyclerView()
+    }
+
+    private fun iniciarRecyclerView() {
+//para o layout ficar horizontal
+//        binding.recyclerViewJogos.layoutManager = LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false)
+        binding.recyclerViewJogos.layoutManager = LinearLayoutManager(this)
+        binding.recyclerViewJogos.adapter = JogosAdapter(Datasource.getJogos())
     }
 }
--- app/src/main/java/com/app/adapter/JogosAdapter.kt
+++ app/src/main/java/com/app/adapter/JogosAdapter.kt
@@ -6,28 +6,34 @@ import androidx.recyclerview.widget.RecyclerView
 import com.app.databinding.ItemListaJogosBinding
 import com.app.model.Jogo
 
+// Classe principal do adaptador que estende RecyclerView.Adapter
 class JogosAdapter(private val listaJogos: ArrayList<Jogo>) : RecyclerView.Adapter<JogosAdapter.JogoViewHolder>() {
 
+    // Método chamado quando o RecyclerView precisa criar um novo ViewHolder
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): JogoViewHolder {
+        // Infla o layout do item usando ViewBinding
         val binding = ItemListaJogosBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return JogoViewHolder(binding)
+        return JogoViewHolder(binding) // Retorna um novo JogoViewHolder com o binding inflado
     }
 
+    // Método chamado pelo RecyclerView para exibir dados em uma posição específica
     override fun onBindViewHolder(holder: JogoViewHolder, position: Int) {
-        val jogo = listaJogos[position]
-        holder.bind(jogo)
+        val jogo = listaJogos[position] // Obtém o objeto Jogo da lista baseado na posição
+        holder.bind(jogo) // Chama o método bind do JogoViewHolder para vincular os dados ao ViewHolder
     }
 
+    // Método que retorna o número total de itens na lista de jogos
     override fun getItemCount(): Int {
         return listaJogos.size
     }
 
+    // Classe interna que representa o ViewHolder para cada item na lista
     class JogoViewHolder(private val binding: ItemListaJogosBinding) : RecyclerView.ViewHolder(binding.root) {
-
+        // Método que vincula os dados do objeto Jogo aos elementos do layout usando ViewBinding
         fun bind(jogo: Jogo) {
-            binding.textConsole.text = jogo.console
-            binding.nomeJogoLista.text=jogo.nomeJogo
-            binding.notaJogo.rating=jogo.notaJogo
+            binding.textConsole.text = jogo.console // Define o texto do console do jogo
+            binding.nomeJogoLista.text = jogo.nomeJogo // Define o nome do jogo
+            binding.notaJogo.rating = jogo.notaJogo // Define a nota do jogo (rating)
         }
     }
 }
