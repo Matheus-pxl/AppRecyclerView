0065b4f4f451b683e8338ccb0295865129c4c04e
Econsoft <matheushellf@gmail.com>
2024-07-05 08:33:40 -0300

    mudança na tabela de inserção e ordenação decrescente na lista

--- app/src/main/java/com/app/model/Jogo.kt
+++ app/src/main/java/com/app/model/Jogo.kt
@@ -1,10 +1,10 @@
 package com.app.model
 
 data class Jogo(
-    var id: Int,
-    var titulo: String,
-    var produtora:String,
-    var notaJogo: Float,
-    var console: String,
+    var id: Int=0,
+    var titulo: String="",
+    var produtora:String="",
+    var notaJogo: Float=0.0f,
+    var console: String="",
     var zerado:Boolean=false
 )
